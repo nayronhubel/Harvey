5be7fbe337b9eaf6d337c1a0bc745729a0a1b866
nayronhubel <37023678+nayronhubel@users.noreply.github.com>
2018-03-04 18:18:50 -0300

    papagaio
    Criado um papagaio que repete o que é inserido no chat

--- app.py
+++ app.py
@@ -26,8 +26,9 @@ def receive_message():
 				if message.get('message'):
 					#Facebook Messenger ID for user so we know where to send response back to
 					recipient_id = message['sender']['id']
+					income = message['message']
 					if message['message'].get('text'):
-						response_sent_text = get_message()
+						response_sent_text = get_message(income['text'])
 						send_message(recipient_id, response_sent_text)
 						#if user sends us a GIF, photo,video, or any other non-text item
 					if message['message'].get('attachments'):
@@ -44,10 +45,8 @@ def verify_fb_token(token_sent):
 
 
 #chooses a random message to send to the user
-def get_message():
-	sample_responses = ["Olá! Ainda estou em fase de desenvolvimento", "Desculpe, ainda estou sendo construído", "Saudações! Em breve estarei pronto para conversarmos"]
-	# return selected item to the user
-	return random.choice(sample_responses)
+def get_message(income):
+	return income
 
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
