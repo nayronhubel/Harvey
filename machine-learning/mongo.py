1d05ddd97e7aace18e7baca8daa30acfd3bc063a
felipe-moreno <felipemorenolopes@gmail.com>
2018-03-24 16:18:28 -0300

    Geração prévia de AIML

--- machine-learning/mongo.py
+++ machine-learning/mongo.py
@@ -1,14 +1,23 @@
+import re
+import spacy
 import pymongo
 from pymongo import MongoClient
-import spacy
 
-nlp = spacy.load('pt')
+nlp = spacy.load('pt_core_news_sm')
 client = MongoClient('mongodb://localhost:27017/Harvey')
 db = client['Harvey']
 collection = db['FAQ']
-res = collection.find();
+res = collection.find({})
 
 for pageView in res :
     print("1")
     doc = nlp(pageView["question"])
-    print([(w.text, w.pos_) for w in doc])
+    aiml = '*'
+    for w in doc:
+        if ((w.pos_ == 'VERB') or (w.pos_ == 'NOUN')):
+            aiml = aiml + ' ' + w.text + ' '
+            #print(w.text)
+            #print("*\n" + w.text)
+        elif (not(aiml[-1:] == '*')):
+            aiml = aiml + '*'
+    print (re.sub(u'  ' , ' ', aiml))
--- machine-learning/test.py
+++ machine-learning/test.py
@@ -1,7 +1,14 @@
 import spacy
-nlp = spacy.load('pt')
+nlp = spacy.load('pt_core_news_sm')
 print('Digite uma frase')
 frase = input()
 doc = nlp(frase)
 print([(w.text, w.pos_) for w in doc])
-print(doc[1])
+
+#print ([(w.text) for w in doc])
+#print ([(w.pos_) for w in doc])
+
+for w in doc:
+    if (w.pos_ == 'VERB'):
+        print ("Palavra: " + w.text + " Tipo: " + w.pos_)
+        
